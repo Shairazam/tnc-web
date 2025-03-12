d53f8a21aa74f243269b4ce7f2af19d277cdd528
Shairazam <shair.yasini@gmail.com>
2025-03-12 22:38:23 +0500

    animation at image

--- src/app/components/hero-section.js
+++ src/app/components/hero-section.js
@@ -1,10 +1,14 @@
-import Image from "next/image"
-import Link from "next/link"
-import blog from '../../../public/blogbg.png'
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import blog from "../../../public/blogbg.png";
 
 export default function HeroSection() {
   return (
-    <section id="Home" className="relative py-16 md:py-24 px-6 md:px-12 overflow-hidden">
+    <section
+      id="Home"
+      className="relative py-16 md:py-24 px-6 md:px-12 overflow-hidden"
+    >
       <div className="max-w-7xl mx-auto grid md:grid-cols-2 gap-8 items-center">
         <div className="z-10">
           <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-6">
@@ -15,8 +19,9 @@ export default function HeroSection() {
             Trading
           </h1>
           <p className="text-gray-300 mb-8 max-w-lg">
-            Dive into a seamless and secure platform designed for both beginners and seasoned traders. Navigate the
-            dynamic world of cryptocurrencies with TNC Crypto.
+            Dive into a seamless and secure platform designed for both beginners
+            and seasoned traders. Navigate the dynamic world of cryptocurrencies
+            with TNC Crypto.
           </p>
           <div className="flex flex-wrap gap-4">
             <Link
@@ -33,11 +38,41 @@ export default function HeroSection() {
             </Link>
           </div>
         </div>
+        {/* <div className="relative h-[300px] md:h-[400px] lg:h-[500px] w-full">
+          <Image src={blog} alt="Blockchain Cubes" fill className="object-contain  animate-bounce" />
+        </div> */}
         <div className="relative h-[300px] md:h-[400px] lg:h-[500px] w-full">
-          <Image src={blog} alt="Blockchain Cubes" fill className="object-contain" />
+          <div
+            className="absolute inset-0"
+            style={{ animation: "slightBounce 3s ease-in-out infinite" }}
+          >
+            <Image
+              src={blog}
+              alt="Blockchain Cubes"
+              fill
+              className="object-contain"
+              style={{
+                background: `radial-gradient(circle, rgba(135, 145, 1675, 0.4) 2%, transparent 60%)`,
+                zIndex: -1, // Ensure it stays behind the image
+              }}
+            />
+          </div>
         </div>
       </div>
+      {/* image bounce slightly up and down */}
+      <style jsx>{`
+        @keyframes slightBounce {
+          0%,
+          100% {
+            transform: translateY(0);
+          }
+          50% {
+            transform: translateY(
+              -25px
+            ); /* Adjust this value for subtle movement */
+          }
+        }
+      `}</style>
     </section>
-  )
+  );
 }
-
